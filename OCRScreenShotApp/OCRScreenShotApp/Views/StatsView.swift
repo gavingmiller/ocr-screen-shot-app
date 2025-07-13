91ad7f85504fa7c13d88f3ae90d44543e7a7d7ea
Gavin Miller <me@gavinmiller.io>
2025-07-13 14:34:04 -0600

    Fix crash when deleting last photo

--- OCRScreenShotApp/OCRScreenShotApp/Views/StatsView.swift
+++ OCRScreenShotApp/OCRScreenShotApp/Views/StatsView.swift
@@ -297,8 +297,13 @@ struct StatsView: View {
         if let model = photoData.wrappedValue.statsModel {
             StatsDatabase.shared.remove(model)
         }
-        photoItems.remove(at: indices[currentIndex])
+        let index = indices[currentIndex]
         dismiss()
+        DispatchQueue.main.async {
+            if photoItems.indices.contains(index) {
+                photoItems.remove(at: index)
+            }
+        }
     }
 
     private func addToDatabase() {
